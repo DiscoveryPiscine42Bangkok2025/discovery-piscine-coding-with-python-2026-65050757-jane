b19acdc2a0224407ec809192080e3a78cda946e0
Ranlada Sudjarittham <65050757@kmitl.ac.th>
2026-02-12 22:38:09 +0700

    Add result output to multiplication script in cell02/ex03

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -3,6 +3,8 @@
 first_number = input("Enter the first number: ")
 second_number = input("Enter the second number: ")
 result = int(first_number) * int(second_number)
+print(f"{first_number} X {second_number} = {result}")
+
 if result < 0:
     print("The result is negative.")
 elif result > 0:
